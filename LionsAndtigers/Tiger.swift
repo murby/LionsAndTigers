3cda155d92af0c6652f45abef31425e51c79fedc
Richard Murby <richard@fwd-media.com>
2015-03-23 09:02:47 -0400

    First Commit

---
+++ LionsAndtigers/Tiger.swift
@@ -0,0 +1,17 @@
+//
+//  Tiger.swift
+//  LionsAndtigers
+//
+//  Created by Richard Murby on 3/23/15.
+//  Copyright (c) 2015 FWD-Media. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+    var age = 0
+    var name = ""
+    var breed = ""
+    var image = UIImage(named:"")
+}
